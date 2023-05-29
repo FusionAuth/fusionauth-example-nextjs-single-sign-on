c3a5f843387e622109a3aac23dd4c15487589165
Alex <alex@codingcat.dev>
2023-05-29 17:45:41 -0400

    add client auth

--- app/src/pages/api/auth/[...nextauth].js
+++ app/src/pages/api/auth/[...nextauth].js
@@ -9,8 +9,12 @@ export const authOptions = {
             clientSecret: process.env.FUSIONAUTH_CLIENT_SECRET,
             wellKnown: `${process.env.FUSIONAUTH_URL}/.well-known/openid-configuration/${process.env.FUSIONAUTH_TENANT_ID}`,
             tenantId: process.env.FUSIONAUTH_TENANT_ID, // Only required if you're using multi-tenancy
+            client: {
+                authorization_signed_response_alg: 'HS256', // Should match Tenant or Application Setting see https://github.com/FusionAuth/fusionauth-example-nextjs-single-sign-on/issues/1
+                id_token_signed_response_alg: 'HS256'// Should match Tenant or Application Setting
+            }
         }),
-    ],
+    ]
 }
 
 export default NextAuth(authOptions)
